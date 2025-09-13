3c43ade0d2aad9a9f8bb4dba8d787907015330d8
v1adhope <vladislavgardner@gmail.com>
2025-09-13 19:16:43 +0300

    Add healthz rote

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpResponse, HttpServer, Responder, web};
+use std::time::Duration;
+
+async fn healthz() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/healthz", web::get().to(healthz)))
+        .bind("127.0.0.1:8080")?
+        .client_disconnect_timeout(Duration::from_secs(10))
+        .shutdown_timeout(10)
+        .run()
+        .await
 }
