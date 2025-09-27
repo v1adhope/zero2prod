f5b5798c8a568739a4333edcff2ebbac0f9b3b80
v1adhope <vladislavgardner@gmail.com>
2025-09-27 23:47:28 +0300

    Remove application from container

--- tests/healthz.rs
+++ tests/healthz.rs
@@ -30,7 +30,6 @@ async fn spawn_app() -> TestApp {
 
     let mut cfg = get_config().expect("failed to read config");
     cfg.database.database_name = Uuid::now_v7().to_string();
-    cfg.database.host = "127.0.0.1".to_string();
 
     let listener = TcpListener::bind("127.0.0.1:0").expect("failed to bind random port");
     let port = listener.local_addr().unwrap().port();
